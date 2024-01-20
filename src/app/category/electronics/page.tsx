ba540c87d546de3e8f50c1c9e4715db603e84bf6
nnnozimjon <iamnozimjon@gmail.com>
2024-01-20 12:16:31 -0300

    fix: filter

--- src/app/category/electronics/page.tsx
+++ src/app/category/electronics/page.tsx
@@ -9,6 +9,35 @@ import { GridCard } from "@/components/grid-card";
 import { useDisclosure } from "@mantine/hooks";
 
 export default function ElectronicsCategoryPage() {
+  const [filterByCategory, setFilterByCategory] = useState<{ id: number, name: string, isActive: boolean }[]>([{
+    id: 2,
+    name: 'Смартфоны',
+    isActive: false
+  },
+  {
+    id: 3,
+    name: 'Ноутбуки',
+    isActive: false
+  },
+  {
+    id: 4,
+    name: 'Аудиоустройства',
+    isActive: false
+  },
+  {
+    id: 5,
+    name: 'Фотоаппараты',
+    isActive: false
+  },
+  {
+    id: 6,
+    name: 'Носимая техника',
+    isActive: false
+  }
+  ])
+
+  console.log(filterByCategory)
+
   const [view, setView] = useState<'grid' | 'list'>('grid')
   const [opened, { open, close }] = useDisclosure(false);
 
@@ -18,7 +47,21 @@ export default function ElectronicsCategoryPage() {
         <Grid className="mb-4">
           <Grid.Col span={{ lg: 3, sm: 12 }} className="">
             <Categories />
-            <LeftSideFilter className="hidden md:block lg:block" />
+            <LeftSideFilter
+              filterByCategory={{
+                data: filterByCategory,
+                onChange: (e) => {
+                  setFilterByCategory((prev) => {
+                    const updatedCats = [...prev];
+                    const targetIndex = updatedCats.findIndex((cat) => cat.id === Number(e.target.value))
+                    updatedCats[targetIndex].isActive = e.target.checked;
+                    return updatedCats
+                  })
+                }
+              }}
+
+
+              className="hidden md:block lg:block" />
           </Grid.Col>
           <Grid.Col span={{ lg: 9, sm: 12 }} className="mt-0 md:mt-4">
             <Flex className="border border-gray-300 md:rounded-md rounded-none p-2" align={'center'} justify={'space-between'}>
